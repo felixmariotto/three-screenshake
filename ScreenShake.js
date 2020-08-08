5b537353aafc8819a9b1ca5cca7aa9e0b11a652d
weiserhei <falk@thiele-medien.de>
2020-08-09 01:59:43 +0200

    cleanups

--- ScreenShake.js
+++ ScreenShake.js
@@ -1,66 +1,66 @@
 import { Vector3 } from "three";
 
-// This is a quadratic function that return 0 at first, then return 0.5 when t=0.5,
-// then return 0 when t=1 ;
+// This is a quadratic function that return 0 at first,
+// then return 0.5 when t=0.5, then return 0 when t=1.
 function getQuadra(t) {
-	return 9.436896e-16 + (4*t) - (4*(t*t)) ;
+    return 9.436896e-16 + (4*t) - (4*(t*t));
 }
-        
+
 // This creates the wavy movement of the camera along the interval.
 // The first bloc call this.getQuadra() with a positive indice between
 // 0 and 1, then the second call it again with a negative indice between
 // 0 and -1, etc. Variable position will get the sign of the indice, and
 // get wavy.
 function computePosition(interval) {
-	if (interval < 0.4) {
-		return getQuadra(interval / 0.4);
-	} else if (interval < 0.7) {
-		return getQuadra((interval-0.4) / 0.3) * -0.6;
-	} else if (interval < 0.9) {
-		return getQuadra((interval-0.7) / 0.2) * 0.3;
-	}
-	return getQuadra((interval-0.9) / 0.1) * -0.1;
+    if (interval < 0.4) {
+        return getQuadra(interval / 0.4);
+    } else if (interval < 0.7) {
+        return getQuadra((interval-0.4) / 0.3) * -0.6;
+    } else if (interval < 0.9) {
+        return getQuadra((interval-0.7) / 0.2) * 0.3;
+    }
+    return getQuadra((interval-0.9) / 0.1) * -0.1;
 }
 
 
 export default class ScreenShake {
-	constructor(camera) {
+    constructor(camera) {
 
-		// When a function outside ScreenShake handle the camera, it should
-		// always check that enabled is false before.
-		let enabled = false,
-			timestampStart = undefined,
-			timestampEnd = undefined,
-			startPoint = undefined,
-			endPoint = undefined;
+        // When a function outside ScreenShake handle the camera, it should
+        // always check that enabled is false before.
+        let enabled = false;
+        let timestampStart;
+        let timestampEnd;
+        let startPoint;
+        let endPoint;
 
-		// update(camera) must be called in the loop function of the renderer,
-		// it will re-position the camera according to the requested shaking.
-		this.update = function update() {
-			if (enabled === true) {
-				const now = Date.now();
-				if(timestampEnd > now) {
-					const interval = (Date.now() - timestampStart) / 
-						(timestampEnd - timestampStart);
-					// Here the camera is positioned according to the wavy 'position' variable.
-					const position = computePosition(interval);
-					camera.position.lerpVectors(startPoint, endPoint, position);
-				} else {
-					camera.position.copy(startPoint);
-					enabled = false;
-				}
-			}
-		};
+        // update() must be called in the loop function of the renderer,
+        // it will re-position the camera according to the requested shaking.
+        this.update = function update() {
+            if (enabled === true) {
+                const now = Date.now();
+                if(timestampEnd > now) {
+                    const interval = (Date.now() - timestampStart) /
+                        (timestampEnd - timestampStart);
+                    const position = computePosition(interval);
+                    // Here the camera is positioned according to the wavy 'position' variable.
+                    camera.position.lerpVectors(startPoint, endPoint, position);
+                } else {
+                    camera.position.copy(startPoint);
+                    enabled = false;
+                }
+            }
+        };
 
-		// This initialize the values of the shaking.
-		// vecToAdd param is the offset of the camera position at the climax of its wave.
-		this.shake = function shake(vecToAdd, milliseconds) {
-			enabled = true;
-			timestampStart = Date.now();
-			timestampEnd = timestampStart + milliseconds;
-			startPoint = new Vector3().copy(camera.position);
-			endPoint = new Vector3().addVectors(camera.position, vecToAdd);
-		};
+        // This initialize the values of the shaking.
+        // vecToAdd param is the offset of the camera position at the climax of its wave.
+        this.shake = function shake(vecToAdd, milliseconds) {
+            enabled = true;
+            timestampStart = Date.now();
+            timestampEnd = timestampStart + milliseconds;
+            startPoint = new Vector3().copy(camera.position);
+            endPoint = new Vector3().addVectors(camera.position, vecToAdd);
+        };
 
-	}
+    }
 }
